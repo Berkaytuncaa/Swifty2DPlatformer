b6b2fa2beabd36dfb63accf30c939188ed9fbf83
Berkay Tunca <berkaytunca40@gmail.com>
2024-04-15 20:26:12 +0300

    update PlayerController.cs and make anim transition for running and idle

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,83 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    private float movementInputDirection;
+
+    private Animator _anim;
+    private bool _isRunning;
+
+    private float _movementInputDirection;
+    private bool _isFacingRight = true;
+
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpForce;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+        CheckInput();
+        CheckMovementDirection();
+        UpdateAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckMovementDirection()
+    {
+        if (_isFacingRight && _movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!_isFacingRight && _movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if (_rb.velocity.x != 0)
+        {
+            _isRunning = true;
+        }
+        else
+        {
+            _isRunning = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        _anim.SetBool("_isRunning", _isRunning);
+    }
+
+    private void CheckInput()
+    {
+        _movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        _rb.velocity = new Vector2(movementSpeed * _movementInputDirection, _rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
     }
 }
