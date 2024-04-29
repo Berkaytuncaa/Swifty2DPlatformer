759a89098996f89b2767c906ac535da235d566e0
Berkay Tunca <berkaytunca40@gmail.com>
2024-04-29 19:29:52 +0300

    update scene + add TODO

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 // TODO: Player can get into the platform tile. -probably cuz of falling speed-
+// TODO: PLayer has an animation bug, due to _isRunning bool.(i believe)
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
