596a7622019fb99b87a0e29d7d9b5892098cd424
Berkay Tunca <berkaytunca40@gmail.com>
2024-06-02 01:23:13 +0300

    implement camera transation

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera vcam1;
+    [SerializeField] private CinemachineVirtualCamera vcam2;
+    [SerializeField] private GameObject blockObject;
+
+    void SwitchCamera()
+    {
+        if (vcam1.Priority > vcam2.Priority)
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 10;
+            blockObject.SetActive(true);
+        }
+        else
+        {
+            vcam1.Priority = 10;
+            vcam2.Priority = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SwitchCamera();
+        }
+    }
+}
