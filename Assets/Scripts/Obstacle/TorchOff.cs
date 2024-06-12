96568fc0514b42b6d1373b6920bb9c24fec6d5d4
Berkay Tunca <berkaytunca40@gmail.com>
2024-06-12 04:26:25 +0300

    update TorchOff script

--- Assets/Scripts/Obstacle/TorchOff.cs
+++ Assets/Scripts/Obstacle/TorchOff.cs
@@ -8,10 +8,8 @@ public class TorchOff : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Find all Torch objects in the scene
             Torch[] allTorches = FindObjectsOfType<Torch>();
 
-            // Set isBurning to false for all Torch objects
             foreach (Torch torch in allTorches)
             {
                 torch.isBurning = false;
