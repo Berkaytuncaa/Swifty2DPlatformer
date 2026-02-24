828fe2db7d50aaa85cbc2f3bd26609f3e8d84944
BerkayTunca <berkaytunca40@gmail.com>
2026-02-25 02:18:25 +0300

    recreate skull boss normal

---
+++ Assets/Scripts/Obstacle/SkullBossNormal.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullBossNormal : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float zigzagAmplitude = 1.0f;
+    [SerializeField] private float zigzagFrequency = 2.0f;
+    [SerializeField] private LayerMask platformLayer;
+
+    private bool movingRight = true;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveZigzag();
+    }
+
+    private void MoveZigzag()
+    {
+        float horizontalMovement = moveSpeed * Time.deltaTime;
+        float verticalMovement = Mathf.Sin(Time.time * zigzagFrequency) * zigzagAmplitude * Time.deltaTime;
+
+        transform.Translate(new Vector3(horizontalMovement * (movingRight ? 1 : -1), verticalMovement, 0));
+
+        if (movingRight)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (!movingRight)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & platformLayer) != 0)
+        {
+            movingRight = !movingRight;
+        }
+    }
+}
